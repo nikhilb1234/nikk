bb508bf610f92c712d36d799b46b884d153f1749
nikhilb1234 <nikkinikhil635@gmail.com>
2024-12-29 20:11:24 +0530

    Add files via upload

---
+++ 20241229-193811.up.py
@@ -0,0 +1,11 @@
+username=input("Enter the username:")
+password=input("Enter the password:")
+if username=="Admin" and password=="gptk@123":
+    print("welcome Admin")
+    print("Good Morning,Have a great day.")
+elif username=="nikhil" and password=="nikhil@123":
+    print("welcome nikhil")
+    print("Good Morning,Have a great day.")  
+else:
+    print("invalid username or password")
+    
