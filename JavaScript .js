100ce0f0fa2f6ccb0b1d2f68f68e2f66d36c0609
nikhilb1234 <nikkinikhil635@gmail.com>
2025-01-29 17:08:45 +0530

    Add files via upload

---
+++ JavaScript .js
@@ -0,0 +1,9 @@
+<meta name='viewport' content='width=device-width, initial-scale=1'/><script>// Function to print "Hello, World!"
+function sayHello() {
+    return "Hello, World!";
+}
+
+// Calling the function
+let message = sayHello();
+console.log(message);
+</script>
